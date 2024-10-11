13231da8b1ff6a26a2962696aa56feaad6aec4bf
Levent Ozturk <lozturkk987@gmail.com>
2024-10-11 16:04:54 -0400

    add edge driver & remove webdriver dependency

--- src/test/java/com/company/utilities/DriverFactory.java
+++ src/test/java/com/company/utilities/DriverFactory.java
@@ -1,9 +1,9 @@
 package com.company.utilities;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,25 +12,21 @@ import java.util.function.Supplier;
 @Log4j2
 public class DriverFactory {
 
-    private static final Supplier<WebDriver> chromeSupplier = () -> {
-        WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
-    };
+    private static final Supplier<WebDriver> chromeSupplier = ChromeDriver::new;
+    private static final Supplier<WebDriver> firefoxSupplier = FirefoxDriver::new;
+    private static final Supplier<WebDriver> edgeSupplier = EdgeDriver::new;
 
-    private static final Supplier<WebDriver> firefoxSupplier = () -> {
-        WebDriverManager.firefoxdriver().setup();
-        return new FirefoxDriver();
-    };
 
-    private static final Map<String, Supplier<WebDriver> > DRIVERPOOL = new HashMap<>();
+    private static final Map<String, Supplier<WebDriver> > DRIVER_POOL = new HashMap<>();
 
     static {
-        DRIVERPOOL.put("chrome",chromeSupplier);
-        DRIVERPOOL.put("firefox",firefoxSupplier);
+        DRIVER_POOL.put("chrome",chromeSupplier);
+        DRIVER_POOL.put("firefox",firefoxSupplier);
+        DRIVER_POOL.put("edge",edgeSupplier);
     }
 
     public static WebDriver getDriver(String browser){
-        return DRIVERPOOL.get(browser).get();
+        return DRIVER_POOL.get(browser).get();
     }
 
 
